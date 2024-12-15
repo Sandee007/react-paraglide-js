8987c003aacfcf091770d377f3282411c36d2e3e
Sandee007 <sandeepaalahakoon@gmail.com>
2024-12-15 20:10:07 +0530

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { paraglide } from "@inlang/paraglide-vite";
+import path from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    paraglide({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+		}),
+  ],
+  resolve: {
+		alias: {
+			$paraglide: path.resolve(__dirname, "./src/paraglide"),
+		},
+	},
+});
